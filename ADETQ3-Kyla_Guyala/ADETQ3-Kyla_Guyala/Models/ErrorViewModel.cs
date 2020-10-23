4484aa816c804234d2a57b6c3aa0ca6b6e6701b2
RANCE AUDRIC SANTOS <1247-18@itmlyceumalabang.onmicrosoft.com>
2020-10-23 23:19:32 +0800

    Commit a message

---
+++ ADETQ3-Kyla_Guyala/ADETQ3-Kyla_Guyala/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ADETQ3_Kyla_Guyala.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
